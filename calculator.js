29ec5404902da02645380d66e6506c43b3db2898
Manish <manihs@gmail.com>
2020-09-09 18:45:09 +0545

    Working JS, click event on inputElement

--- calculator.js
+++ calculator.js
@@ -4,118 +4,178 @@ const operationValueElement = document.querySelector(".operation .value");
 const resultValueElement = document.querySelector(".result .value");
 
 //Created Object Property which is inside Array
-let calculatorButtons = [{
-   name: "delete",
-   symbol: "⌫",
-   formula: false,
-   type: "key"
-}, {
-   name: "clear",
-   symbol: "C",
-   formula: false,
-   type: "key"
-}, {
-   name: "percent",
-   symbol: "%",
-   formula: "/100",
-   type: "number"
-}, {
-   name: "division",
-   symbol: "÷",
-   formula: "/",
-   type: "operator"
-}, {
-   name: "7",
-   symbol: 7,
-   formula: 7,
-   type: "number"
-}, {
-   name: "8",
-   symbol: 8,
-   formula: 8,
-   type: "number"
-}, {
-   name: "9",
-   symbol: 9,
-   formula: 9,
-   type: "number"
-}, {
-   name: "multiplication",
-   symbol: "x",
-   formula: "*",
-   type: "operator"
-}, {
-   name: "4",
-   symbol: 4,
-   formula: 4,
-   type: "number"
-}, {
-   name: "5",
-   symbol: 5,
-   formula: 5,
-   type: "number"
-}, {
-   name: "6",
-   symbol: 6,
-   formula: 6,
-   type: "number"
-}, {
-   name: "addition",
-   symbol: "+",
-   formula: "+",
-   type: "operator"
-}, {
-   name: "1",
-   symbol: 1,
-   formula: 1,
-   type: "number"
-}, {
-   name: "2",
-   symbol: 2,
-   formula: 2,
-   type: "number"
-}, {
-   name: "3",
-   symbol: 3,
-   formula: 3,
-   type: "number"
-}, {
-   name: "subtraction",
-   symbol: "–",
-   formula: "-",
-   type: "operator"
-}, {
-   name: "0",
-   symbol: 0,
-   formula: 0,
-   type: "number"
-}, {
-   name: "comma",
-   symbol: ".",
-   formula: ".",
-   type: "number"
-}, {
-   name: "calculate",
-   symbol: "=",
-   formula: "=",
-   type: "calculate"
-}];
+let calculatorButtons = [
+  {
+    name: "delete",
+    symbol: "⌫",
+    formula: false,
+    type: "key",
+  },
+  {
+    name: "clear",
+    symbol: "C",
+    formula: false,
+    type: "key",
+  },
+  {
+    name: "percent",
+    symbol: "%",
+    formula: "/100",
+    type: "number",
+  },
+  {
+    name: "division",
+    symbol: "÷",
+    formula: "/",
+    type: "operator",
+  },
+  {
+    name: "7",
+    symbol: 7,
+    formula: 7,
+    type: "number",
+  },
+  {
+    name: "8",
+    symbol: 8,
+    formula: 8,
+    type: "number",
+  },
+  {
+    name: "9",
+    symbol: 9,
+    formula: 9,
+    type: "number",
+  },
+  {
+    name: "multiplication",
+    symbol: "x",
+    formula: "*",
+    type: "operator",
+  },
+  {
+    name: "4",
+    symbol: 4,
+    formula: 4,
+    type: "number",
+  },
+  {
+    name: "5",
+    symbol: 5,
+    formula: 5,
+    type: "number",
+  },
+  {
+    name: "6",
+    symbol: 6,
+    formula: 6,
+    type: "number",
+  },
+  {
+    name: "addition",
+    symbol: "+",
+    formula: "+",
+    type: "operator",
+  },
+  {
+    name: "1",
+    symbol: 1,
+    formula: 1,
+    type: "number",
+  },
+  {
+    name: "2",
+    symbol: 2,
+    formula: 2,
+    type: "number",
+  },
+  {
+    name: "3",
+    symbol: 3,
+    formula: 3,
+    type: "number",
+  },
+  {
+    name: "subtraction",
+    symbol: "–",
+    formula: "-",
+    type: "operator",
+  },
+  {
+    name: "0",
+    symbol: 0,
+    formula: 0,
+    type: "number",
+  },
+  {
+    name: "comma",
+    symbol: ".",
+    formula: ".",
+    type: "number",
+  },
+  {
+    name: "calculate",
+    symbol: "=",
+    formula: "=",
+    type: "calculate",
+  },
+];
 
 function createCalculatorButton() {
-   const btnPerRow = 4;
-   let addedBtn = 0;
+  const btnPerRow = 4;
+  let addedBtn = 0;
 
-   calculatorButtons.forEach((button, index) => {
-      //0/4, 1/4, 2/4, 3/4, 4/4, 5/4--18/4.
-      if (addedBtn  % btnPerRow == 0) {
-         inputElement.innerHTML += `<div class="row"></div>`;
-      }
+  calculatorButtons.forEach((button, index) => {
+    //0/4, 1/4, 2/4, 3/4, 4/4, 5/4--18/4.
+    if (addedBtn % btnPerRow == 0) {
+      inputElement.innerHTML += `<div class="row"></div>`;
+    }
 
+    const row = document.querySelector(".row:last-child");
+    row.innerHTML += `<button id="${button.name}">${button.symbol}</button>`;
 
-      const row = document.querySelector(".row:last-child");
-      row.innerHTML += `<button id="${button.name}">${button.symbol}</button>`;
-
-      addedBtn++;
-   });
+    addedBtn++;
+  });
 }
 createCalculatorButton();
+
+//ClickEvent (inputElement)
+inputElement.addEventListener("click", (event) => {
+  const targetElement = event.target;
+  //match targetID and ArrayList name(calculatorButtons)
+  calculatorButtons.forEach((button) => {
+    if (targetElement.id === button.name) {
+      calculator(button);
+    }
+  });
+});
+
+//
+//Creating empty array inside object
+let data = {
+  operation: [],
+  result: [],
+};
+
+//
+//calculator Function
+function calculator(button) {
+  if (button.type === "number") {
+    data.operation.push(button.symbol);
+    data.result.push(button.formula);
+  } else if (button.type === "operator") {
+    data.operation.push(button.symbol);
+    data.result.push(button.formula);
+  } else if (button.type === "key") {
+    if (button.name === "clear") {
+      data.operation = [];
+      data.result = [];
+      updateOutputResult(0);
+    } else if (button.name === "del") {
+      data.operation.pop();
+      data.result.pop();
+    }
+  } else if (button.type === "calculate") {
+  }
+  updateOutputOperation(data.operation.join(""));
+}
